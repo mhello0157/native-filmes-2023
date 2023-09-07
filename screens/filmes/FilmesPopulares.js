d878d30c5406375a441177a2a27ee1f66e6e9885
Orion Teles de Mesquita <orionteles@gmail.com>
2023-09-06 21:55:55 -0300

    Lista de Filmes

--- screens/filmes/FilmesPopulares.js
+++ screens/filmes/FilmesPopulares.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
+import React, { useState } from 'react'
+import { Button, Card, Text } from 'react-native-paper'
+import apiFilmes from '../../services/apiFilmes'
 
 const FilmesPopulares = () => {
+
+  const [filmes, setFilmes] = useState([])
+
+  function carregar() {
+    apiFilmes.get('/movie/popular?language=pt-BR').then(resultado => {  
+      setFilmes(resultado.data.results)
+    })
+  }
+
   return (
-    <Text>FilmesPopulares</Text>
+    <>
+      <Button onPress={carregar}>Carregar Filmes</Button>
+
+      {filmes.map(item => (
+        <Card>
+          <Card.Cover source={{ uri: 'https://image.tmdb.org/t/p/w500/' + item.backdrop_path }} />
+          <Card.Content>
+            <Text variant="titleLarge">{item.title}</Text>
+            <Text variant="bodyMedium">{item.overview}</Text>
+          </Card.Content>
+        </Card>
+      ))}
+    </>
   )
 }
 
